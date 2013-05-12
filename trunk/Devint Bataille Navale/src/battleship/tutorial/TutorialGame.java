aa7724aa12b46cf761502305752065a69e4edcf8
hadrien.luttiau@gmail.com <hadrien.luttiau@gmail.com@8beedd93-722f-951f-ec61-8902f8781120>
2013-05-12 13:24:37 +0000

    Création de la classe KeyboardTutorial Actuellement affiche le plateau et joue le meme role que le keyBoardPlacement

--- trunk/Devint Bataille Navale/src/battleship/tutorial/TutorialGame.java
+++ trunk/Devint Bataille Navale/src/battleship/tutorial/TutorialGame.java
@@ -5,20 +5,36 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 
 import battleship.config.Config;
+import battleship.game.Game;
+import battleship.services.sounds.Phrase;
+import battleship.services.sounds.SoundPlayer;
+import battleship.view.KeyBoardTutorial;
+import battleship.view.KeyboardBattle;
+import battleship.view.KeyboardPlacement;
 
-public class TutorialGame extends StateBasedGame {
-
+public class TutorialGame extends Game {
+	private SoundPlayer soundPlayer;
+//	private KeyboardBattle kbbView;
+//	private KeyboardPlacement kbpView;
+	private KeyBoardTutorial kbtView;
+	
 	public TutorialGame() 
 	{
-		super(Config.WINDOW_TITLE);
+		super();
+		this.soundPlayer = new SoundPlayer(this);
+		Phrase.SOUND_PLAYER = this.soundPlayer;
+
+		//this.kbbView = new KeyboardBattle(Config.WINDOW_HEIGHT,Config.WINDOW_WIDTH, this);
+		//this.kbpView = new KeyboardPlacement(Config.WINDOW_HEIGHT,Config.WINDOW_WIDTH, this);
+		this.kbtView = new KeyBoardTutorial(Config.WINDOW_HEIGHT,Config.WINDOW_WIDTH, this);
+		this.isSoundEnabled = true;
 		
 	}
 
 	@Override
 	public void initStatesList(GameContainer arg0) throws SlickException 
 	{
-		
-		
+		this.addState(this.kbtView);
 	}
 
 
